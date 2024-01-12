e4c127507c30305c7204f359b837e398f43ce8ad
Yssaky Assad Luz <assadyssaky@gmail.com>
2024-01-11 21:56:45 -0300

    Adicionado Evento JavaScript para animar imagem da sessão de compra de planos;

---
+++ script.js
@@ -0,0 +1,3 @@
+//Codigo JavaScript Que adiciona Movimento a Imagem da Sessão de Informações Sobre Plano
+
+document.addEventListener('DOMContentLoaded', function () { window.setTimeout(document.querySelector('svg').classList.add('animated'), 1000); })
