bb48550d4f077a290d03cb35abac9e1cbfa8ab33
Yssaky Assad Luz <142748646+Assad-Lz@users.noreply.github.com>
2024-01-15 19:42:36 +0000

    Sessao de informações adicionadas

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 //Codigo JavaScript Que adiciona Movimento a Imagem da Sessão de Informações Sobre Plano
 
 document.addEventListener('DOMContentLoaded', function () { window.setTimeout(document.querySelector('svg').classList.add('animated'), 1000); })
+
+//Codigo JavaScript Que adiciona Movimento a Imagem da Sessão de Informações Sobre VPN+
+document.addEventListener('DOMContentLoaded', function () {window.setTimeout(document.querySelector('svg').classList.add('animated'),1000);})
+
+//Codigo JavaScript Que adiciona Movimento a Imagem da Sessão de Informações Sobre Proxy
+document.addEventListener('DOMContentLoaded', function () {window.setTimeout(document.querySelector('svg').classList.add('animated'),1000);})
+
+//Codigo JavaScript Que adiciona Movimento a Imagem da Sessão de Informações Sobre Firewall
+document.addEventListener('DOMContentLoaded', function () {window.setTimeout(document.querySelector('svg').classList.add('animated'),1000);})
